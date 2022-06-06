fd8eb8ba04684d9c812de0da25820f0b751af186
Anil5520 <anilmahto812@gmail.com>
2022-06-06 12:54:29 +0530

    bookSchema assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,3 +13,19 @@ const getUsersData= async function (req, res) {
 
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
+
+
+
+const createBook= async function (req, res) {
+    let data= req.body
+    let savedBook= await UserModel.create(data)
+    res.send({msg: savedBook})
+}
+
+const getBookData= async function (req, res) {
+    let allBooks= await UserModel.find()
+    res.send({msg: allBooks})
+}
+
+module.exports.createBook= createBook
+module.exports.getBookData= getBookData
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Anil5520:Anil%402000@cluster0.1zvfs.mongodb.net/Anil1234-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
